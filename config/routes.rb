cf469788040e265ce81f107f6269f37793e7bb5b
Will Alvarez <noxwill@gmail.com>
2015-02-02 21:40:32 -0600

    rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :products, only: [:show, :index]
 
